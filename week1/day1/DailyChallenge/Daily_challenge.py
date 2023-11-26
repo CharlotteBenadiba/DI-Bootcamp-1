c419fb487622509dc0317f58f1694fc5037c44d9
ana.skaliy <skaliy.ana1@gmail.com>
2023-11-26 16:14:55 +0200

    first commit

---
+++ week1/day1/DailyChallenge/Daily_challenge.py
@@ -0,0 +1,20 @@
+# strg = str(input('Write your string. The string must be 10 characters long'))
+# if len(strg)<10:
+#     print('string not long enough')
+# elif len(strg)>10:
+#     print('string too long')
+# else:
+#     print('perfect string')
+
+# print(strg[0])
+# print(strg[-1])
+
+# for i in range(0,len(strg)):
+#     print(strg[0:i+1])
+
+# import random
+# strg = list(strg)
+# random.shuffle(strg)
+# for i in strg:
+#     print (i, end = '')
+# print()
---
+++ week1/day1/ExercisesXP/XP.py
@@ -0,0 +1,53 @@
+#1
+
+# print('Hello world\n'*4)
+
+# #2
+# x = (99**3)*8
+# print(x)
+
+#3
+# False
+# True
+# False
+# False
+# False
+
+#4
+# Computer_brand = 'MacBook'
+# print('I have a {}'.format(Computer_brand))
+
+#5
+# name = 'Anastasia'
+# age = 26 
+# shoe_size = 37 
+# info = 'My name is {}, I am {} years old, and my shoe size is {}.'.format(name,age,shoe_size)
+# print(info)
+
+#6
+# a = 10
+# b = 5
+# if a > b :
+#     print('Hello World')
+
+#7
+# number = int(input('Write number'))
+# if number %2 :
+#     print ("odd number")
+# else :
+#     print ('even number')
+
+#8
+# my_name = 'Anastasia'
+# users_name = str(input('What is your name?'))
+# if my_name == users_name:
+#     print ('Wow! We have the same names!')
+# else :
+#     print ('What a pity thar our names are not the same.')
+
+#9
+# height = int(input('What is your height in inches?'))
+# if height >= 145 :
+#     print('You are tall enough to ride.')
+# else:
+#     print ('You need to grow some more to ride.')    
