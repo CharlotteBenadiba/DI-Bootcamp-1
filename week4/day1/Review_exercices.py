9401a22c76ecdfdf84e1c50c0934137246ed4c7c
ana.skaliy <skaliy.ana1@gmail.com>
2023-12-17 14:54:06 +0200

    day1review

---
+++ week4/day1/Review_exercices.py
@@ -0,0 +1,43 @@
+#Conditions
+#1
+# word1 = input('Enter the first word:')
+# word2 = input('Enter the second word:')
+# word3 = input('Enter the third word:')
+# sentence = f'The words you entered are: {word1}, {word2} and {word3}'
+# print(sentence)
+#2
+# list = ['hello', 'world']
+# str1 = list[1].upper()
+# print(str1)
+# str2 = list[0][::-1]
+# print(str2)
+#3
+# names = ['Alice','Max','David','Anastasia']
+# print(names[::2])
+#4
+# my_numbers = [1,2,3,4,5]
+# user_numbers = int(input('Enter your number:'))
+# my_numbers.insert(-1,user_numbers)
+# print(my_numbers)
+#5
+# my_data = {
+#     'first_name' : 'Anastasia',
+#     'last_name' :'Skaliy',
+#     'age' : 26,
+#     'gender' : 'female',
+#     'fav_food' : 'sushi'
+# }
+# print(my_data)
+#6
+# alc_level = float(input('Please enter your blood alcohol level'))
+# if alc_level >0.5 :
+#     print(' A user is allowed to drive home if their blood alcohol is less than 0.5 %. Please take a cab')
+# else:
+#     print('You are allowed to drive.')   
+#7
+# user_gender = input('Please chooce your gender (Male/Female)')
+# user_age = int(input('Please enter your age'))
+# if (user_gender.lower() =='male' and user_age >65) or (user_gender.lower() == 'female' and user_age >60):
+#     print('It\'s time to retire!')
+# else:
+#     print('It\'s not time to retire yet')  
---
+++ week4/day1/w4d1.py
@@ -0,0 +1,82 @@
+# x = 7 
+# y = 5 
+# # print(x+y) 
+
+# greeting = 'Hello world'
+# print(greeting[2]) #start from 0 
+# print(greeting[-1]) # last char
+# print(greeting[2:5]) #l l o 
+
+# password = input("Please input ur password")
+# if len(password) < 8 :
+#     print ("Password is not long enough")
+# elif len(password) >24:
+#     print ("Password too long")   
+# else:
+#     print('Great password')
+
+#Taxes 
+# income = int(input("Please input your income"))
+# if income < 10000:
+#     print ("Your tax rate = 0")
+# elif income > 10000 and income < 15000:
+#     tax = 0.05 *(income - 10000)
+#     print(tax)
+# else:
+#     tax = 0.05 *(15000 - 10000) + 0.10 * (income-15000)
+#     print(tax)      
+
+# mylist = [2,3,56,45,67]
+# for number in mylist:
+#     print(number + 2) # each number + 2 and then each number * 2 
+#     print(number * 2)
+# print('done')    
+
+# counter = 1 # runs runs runs till it True
+# while counter <= 10 :
+#     print(f'the count is {counter}')
+#     counter += 1
+
+# for counter in range(20):
+#     if counter %2 == 0:
+#         print(f'The counter is {counter} and it is even.') 
+
+# counter = 0
+# while counter <20:
+#     if counter %2 == 0:
+#         print(f'The counter is {counter} and it is even.') 
+#         counter += 1
+
+#functions 
+# print(len('asdf'))       
+# string = input('Enter ur string')
+# counter = 0 
+# while string[counter] != '.':
+#     counter +=1
+# print(counter)    
+
+# def stringcounter(string : str):
+#     counter = 0 
+#     while string[counter] != '.':
+#         counter +=1
+#     print(counter)    
+# st = input('Enter ur string')
+# stringcounter(st)    
+
+
+# functions with outputs/return values
+# x =int('345')
+# print(x+6)
+
+# length = len('this str')
+
+def sum(num1:int, num2:int):
+    result = num1+num2
+    return result
+
+res = sum(45,68)
+print(res)
+
+result2 = res + 9 
+result2 = sum(res,9)
+print(result2)
